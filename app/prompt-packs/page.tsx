bcb88ec7cc74e1dfd3446d79c5151e0aa9193e98
sholajegede <jegedeshola@gmail.com>
2026-02-03 19:57:00 +0100

    Add prompt pack page

---
+++ app/prompt-packs/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from 'react';
+
+export default function PromptPacks() {
+  return (
+    <>
+      <style jsx global>{`
+        html, body, div#__next {
+          margin: 0;
+          height: 100%;
+          overflow: hidden;
+        }
+        iframe {
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border: 0;
+        }
+      `}</style>
+
+      <iframe 
+        src="https://shipitfirst.notion.site/ebd//2fccb1824ca08004933cfc6834ca59d9"
+        frameBorder="0"
+        title="Notion Page"
+        allowFullScreen
+      />
+    </>
+  );
+}
